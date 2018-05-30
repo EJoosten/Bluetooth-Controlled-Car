cfc4b7f7913ba370507780688187a00dc0a36cc4
EJoosten <39192070+EJoosten@users.noreply.github.com>
2018-05-30 16:17:49 +0200

    Added crane
    Added a second mode for the crane with a switch statement.

--- BT_Control_car.ino
+++ BT_Control_car.ino
@@ -23,59 +23,97 @@ void loop()
   {
     data = Serial.read();        //Read the incoming data & store into data
 
-    switch (data)
+    /*
+     * *****************************************************************************************************
+       Drive Mode
+     * *****************************************************************************************************
+    */
+    if (data == '1')
     {
-      case 'f':                   //Case to move forward
-        digitalWrite(Rled, LOW);
-        digitalWrite(Lled, LOW); 
-        analogWrite(M1Rpwm, 0);
-        analogWrite(M1Lpwm, 255);
-        analogWrite(M2Rpwm, 0);
-        analogWrite(M2Lpwm, 255);
-        break;
+      switch (data)
+      {
+        case 'f':                   //Case to move forward
+          digitalWrite(Rled, LOW);
+          digitalWrite(Lled, LOW);
+          analogWrite(M1Rpwm, 0);
+          analogWrite(M1Lpwm, 255);
+          analogWrite(M2Rpwm, 0);
+          analogWrite(M2Lpwm, 255);
+          break;
 
-      case 'b':                   // case to move backwards
-        digitalWrite(Rled, LOW);
-        digitalWrite(Lled, LOW); 
-        analogWrite(M1Rpwm, 255);
-        analogWrite(M1Lpwm, 0);
-        analogWrite(M2Rpwm, 255);
-        analogWrite(M2Lpwm, 0);
-        break;
+        case 'b':                   // case to move backwards
+          digitalWrite(Rled, LOW);
+          digitalWrite(Lled, LOW);
+          analogWrite(M1Rpwm, 255);
+          analogWrite(M1Lpwm, 0);
+          analogWrite(M2Rpwm, 255);
+          analogWrite(M2Lpwm, 0);
+          break;
 
-      case 'l':                   // case to turn left
-        digitalWrite(Lled, HIGH);
-        digitalWrite(Rled, LOW);
-        analogWrite(M1Rpwm, 0);
-        analogWrite(M1Lpwm, 255);
-        analogWrite(M2Rpwm, 255);
-        analogWrite(M2Lpwm, 0);
-        break;
+        case 'l':                   // case to turn left
+          digitalWrite(Lled, HIGH);
+          digitalWrite(Rled, LOW);
+          analogWrite(M1Rpwm, 0);
+          analogWrite(M1Lpwm, 255);
+          analogWrite(M2Rpwm, 255);
+          analogWrite(M2Lpwm, 0);
+          break;
 
-      case 'r':                   // case to turn right
-        digitalWrite(Lled, LOW); 
-        digitalWrite(Rled, HIGH);
-        analogWrite(M1Rpwm, 255);
-        analogWrite(M1Lpwm, 0);
-        analogWrite(M2Rpwm, 0);
-        analogWrite(M2Lpwm, 255);
-        break;
+        case 'r':                   // case to turn right
+          digitalWrite(Lled, LOW);
+          digitalWrite(Rled, HIGH);
+          analogWrite(M1Rpwm, 255);
+          analogWrite(M1Lpwm, 0);
+          analogWrite(M2Rpwm, 0);
+          analogWrite(M2Lpwm, 255);
+          break;
 
-      case 's':                   // case to stop
-        digitalWrite(Rled, LOW);
-        digitalWrite(Lled, LOW);              
-        analogWrite(M1Rpwm, 0);
-        analogWrite(M1Lpwm, 0);
-        analogWrite(M2Rpwm, 0);
-        analogWrite(M2Lpwm, 0);
-        break;
+        case 's':                   // case to stop
+          digitalWrite(Rled, LOW);
+          digitalWrite(Lled, LOW);
+          analogWrite(M1Rpwm, 0);
+          analogWrite(M1Lpwm, 0);
+          analogWrite(M2Rpwm, 0);
+          analogWrite(M2Lpwm, 0);
+          break;
 
-      default:                    // default case
-        break;
-
-
+        default:                    // default case
+          break;
+      }
     }
 
+    /*
+    * *****************************************************************************************************
+      Crane Mode
+    * *****************************************************************************************************
+    */
+    if (data == '2')
+    {
+      switch (data)
+      {
+        case 'l':                   // case to turn left
+          break;
+
+        case 'r':                   // case to turn right
+          break;
+
+        case 'u':                   // case to move up
+          break;
+
+        case 'd':                   // case to move down
+          break;
+
+        case 'o':                   // case to open the claw
+          break;
+
+        case 'c':                   // case to close the claw
+          break;
+
+        default:                    // default case
+          break;
+
+      }
+    }
   }
 }
 
